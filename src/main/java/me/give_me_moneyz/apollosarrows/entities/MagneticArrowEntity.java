2b63f02f4bab971c06f291a793948c62323ca762
GitCat3 <hulkbuster119@gmail.com>
2023-02-17 18:52:01 -0500

    added functionality to madnetic arrow

--- src/main/java/me/give_me_moneyz/apollosarrows/entities/MagneticArrowEntity.java
+++ src/main/java/me/give_me_moneyz/apollosarrows/entities/MagneticArrowEntity.java
@@ -3,14 +3,19 @@ package me.give_me_moneyz.apollosarrows.entities;
 import me.give_me_moneyz.apollosarrows.registry.ModItems;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 
 public class MagneticArrowEntity extends AbstractArrow {
+    private AABB aabb;
+
     public MagneticArrowEntity(EntityType<MagneticArrowEntity> entityType, Level world) {
         super(entityType, world);
     }
@@ -31,5 +36,25 @@ public class MagneticArrowEntity extends AbstractArrow {
     @Override
     public Packet<ClientGamePacketListener> getAddEntityPacket() {
         return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+    @Override
+    protected void onHitBlock(BlockHitResult pResult) {
+        super.onHitBlock(pResult);
+        aabb = AABB.ofSize(pResult.getLocation(), 10, 10, 10);
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        if (this.inGround) {
+            if (this.inGroundTime >= 100) {
+                discard();
+                return;
+            }
+            for (Entity entity : this.level.getEntities(this, aabb)) {
+                entity.setDeltaMovement(position().subtract(entity.getPosition(1)));
+            }
+        }
     }
 }
