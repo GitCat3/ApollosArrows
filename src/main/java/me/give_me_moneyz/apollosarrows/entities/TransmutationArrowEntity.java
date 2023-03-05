b547f8131a33ba7d2192a720ff7964edc527f5dc
GitCat3 <hulkbuster119@gmail.com>
2023-03-05 10:38:06 -0500

    made... something for the transmutation arrow, very much does not work yet

--- src/main/java/me/give_me_moneyz/apollosarrows/entities/TransmutationArrowEntity.java
+++ src/main/java/me/give_me_moneyz/apollosarrows/entities/TransmutationArrowEntity.java
@@ -1,9 +1,11 @@
 package me.give_me_moneyz.apollosarrows.entities;
 
+import me.give_me_moneyz.apollosarrows.ApollosArrows;
 import me.give_me_moneyz.apollosarrows.registry.ModItems;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.projectile.AbstractArrow;
@@ -11,9 +13,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.network.NetworkHooks;
 
 public class TransmutationArrowEntity extends AbstractArrow {
+    private BlockPos blockPos;
     public TransmutationArrowEntity(EntityType<TransmutationArrowEntity> entityType, Level world) {
         super(entityType, world);
     }
@@ -37,16 +41,40 @@ public class TransmutationArrowEntity extends AbstractArrow {
     }
 
     @Override
+    protected void onHitBlock(BlockHitResult pResult) {
+        super.onHitBlock(pResult);
+        blockPos = pResult.getBlockPos();
+    }
+
+    @Override
     protected void tickDespawn() {
+        super.tickDespawn();
         if(this.inGroundTime > 40) {
             var itemInHand = ((LivingEntity) getOwner()).getOffhandItem();
-            var amount = itemInHand.getCount();
-            if(amount != 0) {
-                itemInHand.setCount(amount - 1);
+            var blockHandItem = Block.byItem(itemInHand.getItem());
+            if(blockHandItem != Blocks.AIR) {
+                transformBlocks(blockPos, itemInHand);
             }
-            else {
-                discard();
+        }
+    }
+
+    private void transformBlocks(BlockPos pos, ItemStack item) {
+        if(item.getCount() > 0) {
+            var blockHandItem = Block.byItem(item.getItem());
+            if (!level.getBlockState(pos).is(blockHandItem)) {
+                level.setBlockAndUpdate(pos, blockHandItem.defaultBlockState());
+                item.shrink(1);
+                for(Direction direction: Direction.values()) {
+                    var offsetPos = pos.relative(direction);
+                    var blockOffsetPos = level.getBlockState(offsetPos).getBlock();
+                    if(blockOffsetPos != Blocks.AIR) {
+                        transformBlocks(offsetPos, item);
+                    }
+                }
             }
+        }
+        else {
+            discard();
         }
     }
 }
